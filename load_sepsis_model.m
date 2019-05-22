7f5dc48be4fdf677dd16985a925f6e878199441f
bsweely <39773162+bsweely@users.noreply.github.com>
2019-05-22 17:55:09 -0400

    Add files via upload

--- load_sepsis_model.m
+++ load_sepsis_model.m
@@ -1,4 +1,7 @@
 function model = load_sepsis_model()
+    %import decision tree prediciton fct
+    import classreg.learning.classif.CompactClassificationTree.*;
+    
     %load array of trees as random forest model
     loadedObj = load('models.mat');
     model = loadedObj.models;
