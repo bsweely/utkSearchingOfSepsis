2d54693db7600c89262bb70f924a66b25389ffce
bsweely <39773162+bsweely@users.noreply.github.com>
2019-08-21 15:03:14 -0400

    Add files via upload

--- get_sepsis_score.m
+++ get_sepsis_score.m
@@ -1,15 +1,13 @@
 function [score, label] = get_sepsis_score(data, model)
 %include all available data
 selvital = [1:40];
-
 %imputation
 X = data(:,selvital); 
 X = fillmissing(X,'previous');
 X = fillmissing(X,'constant',0);
 
-patient = cell(1,1);
-patient{1} = X;
-X = cell2mat(history(patient, 8));
+padding = 8;
+X = history(X, padding);
 
 scores = predict(model.data, X);
 
